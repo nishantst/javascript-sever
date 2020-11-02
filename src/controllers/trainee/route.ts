dc27c00ad4ad7003c3c86a2236d13e02962d8de7
nishantst <nishant.gupta@successive.tech>
2020-11-02 18:27:59 +0530

    feat: Day 10 -Generic Routes Validations(#39524)

--- src/controllers/trainee/route.ts
+++ src/controllers/trainee/route.ts
@@ -1,13 +1,18 @@
 import * as express from 'express';
 
 import traineeController from './controller';
+import validationHandler from '../../libs/validationHandler';
+import validation from './validation';
 
 const traineeRouter = express.Router();
 
 traineeRouter.route('/')
-.get(traineeController.get)
-.post(traineeController.create)
-.put(traineeController.update)
-.delete(traineeController.delete);
+.get(validationHandler(validation.get), traineeController.get)
+.post(validationHandler(validation.create), traineeController.create)
+.put(validationHandler(validation.update), traineeController.update)
+.delete(validationHandler(validation.delete), traineeController.delete);
+
+traineeRouter.route('/:id')
+.delete(validationHandler(validation.delete), traineeController.delete);
 
 export default traineeRouter;
