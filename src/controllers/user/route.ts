a60ae663f731593a88613e97e9240b9839dac9e1
nishantst <nishant.gupta@successive.tech>
2020-11-06 17:45:15 +0530

    feat:Day 14 - Profile Setup(#40012)

--- src/controllers/user/route.ts
+++ src/controllers/user/route.ts
@@ -1,12 +1,25 @@
 import * as express from 'express';
 import UserController from './controller';
+import validationHandler from '../../libs/validationHandler';
+import validation from './validation';
+import authMiddleWare from '../../libs/routes/authMiddleWare';
 
 const UserRouter = express.Router();
+UserRouter.get('/', authMiddleWare('getUser1', 'read'), validationHandler(validation.get),
+    UserController.get);
 
-UserRouter.route('/')
-.get( UserController.get)
-.post( UserController.create)
-.put( UserController.update)
-.delete( UserController.delete);
+UserRouter.post('/', authMiddleWare('getUser1', 'write'), validationHandler(validation.create),
+    UserController.create);
+
+UserRouter.put('/', authMiddleWare('getUser1', 'write'), validationHandler(validation.update),
+    UserController.update);
+
+UserRouter.delete('/:id', authMiddleWare('getUser1', 'delete'), validationHandler(validation.delete),
+    UserController.delete);
+
+UserRouter.post('/login', validationHandler(validation.login), UserController.login);
+
+UserRouter.get('/me', authMiddleWare('getUser1', 'all'), UserController.me);
+
 
 export default UserRouter;
